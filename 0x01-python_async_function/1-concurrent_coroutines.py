51e3b80c292ccb90d6bddec15c866f096b658d15
Mosesbwire <mosesdev92@gmail.com>
2023-10-10 10:49:54 +0300

    move randsleep function into the wait_random function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,12 +7,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def randsleep(max_delay: int, sorted_list: list[float]) -> None:
-    sleep: float = await wait_random(max_delay)
-    await asyncio.sleep(sleep)
-    sorted_list.append(sleep)
-
-
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """Spwans wait_random n times with the specified max_delay
     Args:
@@ -21,6 +15,13 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     Returns:
         A list of all the delays (float values) in ascending order 
     """
+
     sorted_list: list[float] = []
+
+    async def randsleep(max_delay: int, sorted_list: list[float]) -> None:
+        sleep: float = await wait_random(max_delay)
+        await asyncio.sleep(sleep)
+        sorted_list.append(sleep)
+
     await asyncio.gather(*(randsleep(max_delay, sorted_list) for _ in range(n)))
     return sorted_list
